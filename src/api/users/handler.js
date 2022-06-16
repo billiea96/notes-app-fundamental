395b93c55d83f2623029e13b4aef6cc115e10716
billiea96 <billiea48@gmail.com>
2022-06-16 10:23:47 +0700

    user registration implemented

---
+++ src/api/users/handler.js
@@ -0,0 +1,82 @@
+const ClientError = require("../../exceptions/ClientError");
+
+class UsersHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    this.postUserHandler = this.postUserHandler.bind(this);
+    this.getUserByIdHandler = this.getUserByIdHandler.bind(this);
+  }
+
+  async postUserHandler(req, h) {
+    try {
+      this._validator.validateUserPayload(req.payload);
+
+      const userId = await this._service.addUser(req.payload);
+
+      const response = h.response({
+        status: "success",
+        message: "User berhasil ditambahkan",
+        data: {
+          userId,
+        },
+      });
+
+      response.code(201);
+      return response;
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: "fail",
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+
+      // Server ERROR!
+      const response = h.response({
+        status: "error",
+        message: "Maaf, terjadi kegagalan pada server kami.",
+      });
+      response.code(500);
+      console.error(error);
+      return response;
+    }
+  }
+
+  async getUserByIdHandler(req, h) {
+    try {
+      const { id } = req.params;
+      const user = await this._service.getUserById(id);
+
+      return {
+        status: "success",
+        data: {
+          user,
+        },
+      };
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: "fail",
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+
+      // Server ERROR!
+      const response = h.response({
+        status: "error",
+        message: "Maaf, terjadi kegagalan pada server kami.",
+      });
+      response.code(500);
+      console.error(error);
+      return response;
+    }
+  }
+}
+
+module.exports = UsersHandler;
